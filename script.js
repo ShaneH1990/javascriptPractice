bc851cd097acec50bcf14582764b39c70deb9ef0
ShaneH1990 <shaneharraghy@live.ie>
2022-06-12 15:38:36 +0000

    added footer and randomiser code

--- script.js
+++ script.js
@@ -9,14 +9,15 @@ document.addEventListener("DOMContentLoaded", function(){
     for(let button of buttons){
         //add eventListner for that button to be clicked when it is clicked the code inside the function will then run!
         button.addEventListener("click",function(){
-            //if statment that checks the attribute of the data type to see whtas its value is and if its 'submit' it dsiplays an alert!
+            //if statment that checks the attribute of the data type to see what its value is and if its 'submit' it displays an alert!
             //this. refers to the button that was just clicked!
             if(this.getAttribute("data-type")=== "submit"){
                 createNew(); 
+                
             }
             else if(this.getAttribute("data-type")==="button"){
-                alert("You pressed the 'Next' Button!");
-            }
+               alert("You pressed the 'Next' Button!");
+           }
             else{
                 let normalButton = this.getAttribute("data-type");
             }
@@ -25,15 +26,17 @@ document.addEventListener("DOMContentLoaded", function(){
         function createNew(){
             document.getElementById('imgsetting')
             .style.display = "block";
+           
+        }
+
+        let imageArray = ["rock.jpg", "scissor.jpg","paper.jpg", "spock.jpg","lizard"];
+        let imageLength = imageArray.length;
+
+        function randomiseImg(){
+            let randompic = Math.floor(Math.random() *imageLength);
         }
         
     }
 })
 
-let myArray = ["question1","Question2", "Question3", "Question4"];
-    let arraylength = myArray.length;
-    for(i=0;i<arrayLength;i++){
-        console.log('your question is'+myArray[i]);
-    }
 
-    let myArray2 = ["answer1", "answer2","answer3", "answer4","answer5", "answer6"]
